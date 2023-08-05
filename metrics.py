46be2c45b8cf009ceca45b74cf2ca7396f0804e0
JH Shen <jhs@JHs-MacBook-Pro-3.local>
2023-08-05 12:00:38 -0400

    adding prob based AUC, removing F1, replacing SVM with KNN for efficiency

--- metrics.py
+++ metrics.py
@@ -6,6 +6,7 @@ from sklearn.svm import LinearSVC, SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import xgboost as xgb
 
 from sklearn.decomposition import PCA
@@ -25,23 +26,41 @@ clf_dict = {
     "LR": LogisticRegression,
     "GB": GradientBoostingClassifier,
     "XGB": xgb.XGBClassifier,
-    "SVM": LinearSVC,
+    "KNN": KNeighborsClassifier, 
     "NN": MLPClassifier,
 }
 
 @ignore_warnings(category=ConvergenceWarning)
 def model_choice(clf, xtrain=None, ytrain=None):
-    param_grid = {
+    param_grid_nn = {
         "mlp__alpha": [0.01, 0.05, 0.1],
         "mlp__learning_rate": ["constant", "adaptive"],
         'mlp__hidden_layer_sizes': [(8, 2), (12, 3), (16, 4)] 
+    }
+    param_grid_knn = {
+        "knn__n_neighbors": [3, 5, 7]
     }
     if clf == "XBG":
         model = make_pipeline(
             StandardScaler(), clf_dict[clf](objective="binary:logistic")
         )
-    elif clf == "SVM":
-        model = make_pipeline(StandardScaler(), clf_dict[clf](C=0.2))
+    elif clf == "KNN":
+        model = Pipeline(
+            [
+                ("scalar", StandardScaler()),
+                ("knn", KNeighborsClassifier()),
+            ]
+        )
+        print("running model search")
+        grid_search = GridSearchCV(model, param_grid_knn, n_jobs=-1, cv=5)
+        grid_search.fit(xtrain, ytrain)
+        # final model
+        model = Pipeline(
+            [
+                ("scalar", StandardScaler()),
+                ("knn", KNeighborsClassifier(grid_search.best_params_["knn__n_neighbors"])),
+            ]
+        )
     elif clf == "NN":
         model = Pipeline(
             [
@@ -58,7 +77,7 @@ def model_choice(clf, xtrain=None, ytrain=None):
             ]
         )
         print("running model search")
-        grid_search = GridSearchCV(model, param_grid, n_jobs=-1, cv=5)
+        grid_search = GridSearchCV(model, param_grid_nn, n_jobs=-1, cv=5)
         grid_search.fit(xtrain, ytrain)
         # final model
         model = Pipeline(
@@ -84,53 +103,57 @@ def model_choice(clf, xtrain=None, ytrain=None):
 
 def group_accuracy(correct_arr, group_arr, min_size=10): 
     g_acc_arr = [] 
+    group_bin = {}
     vals, counts = np.unique(group_arr, return_counts=True)
     for g, g_count in zip(vals, counts): 
         if g_count > min_size: 
             g_acc = np.mean(correct_arr[group_arr == g])
             g_acc_arr.append(g_acc)
-    
-    group_bin = {}
-    group_bin["white"] = np.mean(correct_arr[group_arr == 1])
-    group_bin["black"] = np.mean(correct_arr[group_arr == 2])
+            if g == 1: 
+                group_bin["white"] = g_acc
+            elif g == 2: 
+                group_bin["black"] = g_acc
+    # find non-white accuracy
     group_bin["non-white"] = np.mean(correct_arr[group_arr != 1])
     return g_acc_arr, group_bin
 
 def group_f1(target, pred, group_arr, min_size=10): 
     g_f1_arr = [] 
+    group_bin = {}
     vals, counts = np.unique(group_arr, return_counts=True)
     for g, g_count in zip(vals, counts): 
         if g_count > min_size: 
             g_f1 = f1_score(target[group_arr == g], 
                                       pred[group_arr == g])
             g_f1_arr.append(g_f1)
-    group_bin = {}
-    group_bin["white"] = f1_score(target[group_arr == 1], 
-                                      pred[group_arr == 1])
-    group_bin["black"] = f1_score(target[group_arr == 2], 
-                                      pred[group_arr == 2])
+            if g == 1: 
+                group_bin["white"] = g_f1 
+            elif g == 2: 
+                group_bin["black"] = g_f1 
+    
+    # find non-white f1
     group_bin["non-white"] = f1_score(target[group_arr != 1], 
                                       pred[group_arr != 1])
     return g_f1_arr, group_bin
 
 def group_auc(target, pred, group_arr, min_size=10): 
     g_auc_arr = [] 
+    group_bin = {}
     vals, counts = np.unique(group_arr, return_counts=True)
     for g, g_count in zip(vals, counts): 
         if g_count > min_size: 
             # AUC only valid if two classes exist 
-            if (len(np.unique(target[group_arr == g])) > 1) and \
-            (len(np.unique(pred[group_arr == g])) > 1): 
+            if (len(np.unique(target[group_arr == g])) > 1): 
                 g_auc = roc_auc_score(target[group_arr == g], 
                                       pred[group_arr == g])
 
                 g_auc_arr.append(g_auc)
-        
-    group_bin = {}
-    group_bin["white"] = roc_auc_score(target[group_arr == 1], 
-                                      pred[group_arr == 1])
-    group_bin["black"] = roc_auc_score(target[group_arr == 2], 
-                                      pred[group_arr == 2])
+                if g == 1: 
+                    group_bin["white"] = g_auc 
+                elif g == 2: 
+                    group_bin["black"] = g_auc 
+                    
+    # find all non-white AUC                 
     group_bin["non-white"] =roc_auc_score(target[group_arr != 1], 
                                       pred[group_arr != 1])
     
