3570ca057f76e314addb4b1ef427a2c7f389c470
JH Shen <jhs@sr22-7390bd6260.stanford.edu>
2023-09-19 19:14:57 -0700

    adding excess kl code

--- folktables/excess_kl.py
+++ folktables/excess_kl.py
@@ -126,7 +126,6 @@ def run_states(n_runs=1):
 def kl_accuracy(states_df, n_runs=1, n_samples=3000):
     states_mean = states_df.groupby(["state", "clf"]).mean()
     ref_state = "CA"
-    samples = 3000
     year = "2014"
     data_dict = {}
     for state in STATES:
@@ -164,6 +163,8 @@ def kl_accuracy(states_df, n_runs=1, n_samples=3000):
                     (data_dict[ref_state][year]["x"], data_dict[state][year]["x"]), axis=0
                 )
 
+                # estimate kernel density from joint distribution of both both states
+                # results are similar with just the reference state
                 incl = np.asarray(random.sample(range(len(joint_xy)), n_samples))
                 cx, _ = mt.init_density_scale(joint_x[incl])
                 cxy, _ = mt.init_density_scale(joint_xy[incl])
@@ -172,7 +173,8 @@ def kl_accuracy(states_df, n_runs=1, n_samples=3000):
                 pkdex = mt.init_density(data_dict[ref_state][year]["x"][incl], cx)
                 pkdexy = mt.init_density(joint_xy_ref[incl], cxy)
 
-                diff_df = states_mean.loc[ref_state] - states_mean.loc[state]
+                # change in accuracy (negative)
+                diff_df = states_mean.loc[state] - states_mean.loc[ref_state] 
 
                 # sample
                 incl = np.asarray(random.sample(range(len(joint_xy_state)), n_samples))
@@ -191,12 +193,116 @@ def kl_accuracy(states_df, n_runs=1, n_samples=3000):
     return 
                 
 
+def excess_kl(n_runs=1, n_samples=3000):
+    results = []
+
+    year = "2014"
+    # init kde and transform with SD data
+
+    data_dict = {}
+    for state in STATES:
+        data_dict[state] = {}
+        data_source = ACSDataSource(survey_year=year, horizon="1-Year", survey="person")
+        acs_data = data_source.get_data(states=[state], download=True)
+        data_dict[state][year] = {}
+        features, label, group = ACSIncome.df_to_numpy(acs_data)
+        data_dict[state][year]["x"] = features
+        data_dict[state][year]["y"] = label
+        data_dict[state][year]["g"] = np.vectorize(mt.race_grouping.get)(group)
+
+    # must estimate kde based on state (e.g. south dakota)
+    state = "SD"
+    X_train, X_test, y_train, y_test, group_train, _, = train_test_split(
+                    data_dict[state][year]["x"],
+                    data_dict[state][year]["y"],
+                    data_dict[state][year]["g"],
+                    test_size=0.25,
+    )
+    
+    joint_xy = np.concatenate(
+        (X_train, y_train.reshape(-1, 1)),
+        axis=1,
+    )
+    incl = np.asarray(random.sample(range(len(joint_xy)), n_samples))
+
+    size_arr = [len(X_train), 7500] # change these numbers based on state test set 
+    cx, _ = mt.init_density_scale(X_train[incl])
+    cxy, _ = mt.init_density_scale(joint_xy[incl]) 
+    qkdex = mt.init_density(X_test, cx)
+    qkdexy = mt.init_density(np.concatenate((X_test, y_test.reshape(-1, 1)), axis=1), cxy)
+
+    for extra_state in STATES:
+        if extra_state != state:
+            for run in range(n_runs):
+                random.seed(run)
+                X_joint = np.concatenate((X_train, data_dict[extra_state][year]["x"]))
+                y_joint = np.concatenate((y_train, data_dict[extra_state][year]["y"]))
+
+                xy_joint = np.concatenate((X_joint, y_joint.reshape(-1, 1)), axis=1)
+
+                for size in size_arr:
+                # sub sample
+                    if size > n_samples:
+                        incl = np.asarray(random.sample(range(size), n_samples))
+                    else:
+                        incl = range(size)
+
+                    pkdex = mt.init_density(X_joint[:size][incl], cx)
+                    pkdexy = mt.init_density(xy_joint[:size][incl], cxy)
+
+                    kl_x = mt.entropy_input(X_joint[:size][incl], pkdex, qkdex, cx)
+                    kl_xy = mt.entropy_input(xy_joint[:size][incl], pkdexy, qkdexy, cxy)
+
+                    for clf in mt.clf_dict.keys():
+                        model = mt.model_choice(clf, X_joint[:size], y_joint[:size])
+
+                        model.fit(X_joint[:size], y_joint[:size])
+
+                        train_acc = model.score(X_train, y_train)
+
+                        test_acc = model.score(X_test, y_test)
+
+                        results.append(
+                            {
+                                "train_acc": train_acc,
+                                "test_acc": test_acc,
+                                "kl_testx": kl_x,
+                                "kl_testxy": kl_xy,
+                                "size": size,
+                                "run": run,
+                                "clf": clf,
+                                "extra_state": extra_state,
+                            }
+                        )
+
+    results_df = pd.DataFrame(results)
+
+    q1_results = results_df[results_df["size"] == len(X_train)].reset_index()[
+        ["test_acc", "clf", "extra_state", "kl_testx", "kl_testxy"]
+    ]
+    q2_results = results_df[results_df["size"] == 7500].reset_index()[
+        ["test_acc", "clf", "extra_state", "kl_testx", "kl_testxy"]
+    ]
+    diff_results = pd.DataFrame()
+    # accuracy drop n_small - n_large
+    diff_results["acc_diff"] = q2_results["test_acc"] - q1_results["test_acc"]
+    # excess kl n_large - n_small
+    diff_results["klx_diff"] = q2_results["kl_testx"] - q1_results["kl_testx"]
+    diff_results["klxy_diff"] = q2_results["kl_testxy"] - q1_results["kl_testxy"]
+    diff_results["clf"] = q1_results["clf"]
+    diff_results["extra_state"] = q1_results["extra_state"]
+    diff_results.to_csv("../results/excess_kl.csv")
+    return 
+
+
 def main():
     parser = argparse.ArgumentParser(description="Run KL Check")
     
     # Add arguments for the function
     parser.add_argument('--state_kl', dest='state_kl', action='store_true',
                         help='Flag to enable computing state kl.')
+    parser.add_argument('--excess_kl', dest='excess_kl', action='store_true',
+                        help='Flag to enable computing excess kl.')
     parser.set_defaults(mixture=False)  # default value for mixture
     parser.add_argument('--n_runs', type=int, default=1,
                         help='Number of runs.')
@@ -206,15 +312,19 @@ def main():
     # Parse the arguments
     args = parser.parse_args()
     
-    if os.path.isfile("../results/states_results.csv"):
-        states_df = pd.read_csv("../results/states_results.csv")
-    else: 
-        states_df = run_states(n_runs=args.n_runs)
+
     
 
     # Call the function using parsed arguments
     if args.state_kl:
+        if os.path.isfile("../results/states_results.csv"):
+            states_df = pd.read_csv("../results/states_results.csv")
+        else: 
+            states_df = run_states(n_runs=args.n_runs)
+    
         kl_accuracy(states_df, n_runs=args.n_runs, n_samples=args.n_samples)
+    if args.excess_kl:
+        excess_kl(n_runs=args.n_runs, n_samples=args.n_samples)
 
 if __name__ == "__main__":
     main()
